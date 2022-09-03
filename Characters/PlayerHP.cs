c3ef86791a4a5fba74b9fd2b6bec8a419f1e2b16
timgud2111 <timgud2111@gmail.com>
2022-09-03 15:53:04 +0300

    timgud

--- Characters/PlayerHP.cs
+++ Characters/PlayerHP.cs
@@ -5,12 +5,32 @@ using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
+    private void Start()
+    {
+        spriteRend = GetComponent<SpriteRenderer>();
+
+        matBlink = Resources.Load("EnemyBlink", typeof(Material)) as Material;
+        matDefault = spriteRend.material;
+
+        explosion = Resources.Load("Explosion");
+
+        helthDisplay.text = "HP: " + health;
+    }
     public int health;
     public Text helthDisplay;
+
+    private Material matBlink;
+    private Material matDefault;
+    private SpriteRenderer spriteRend;
+
+    private UnityEngine.Object explosion;
     private void Update()
     {
         if(health <=0)
         {
+            GameObject explosionRef = (GameObject)Instantiate(explosion);
+            explosionRef.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
             Destroy(gameObject);
         }
     }
@@ -18,5 +38,11 @@ public class PlayerHP : MonoBehaviour
     {
         health -= damage;
         helthDisplay.text = "HP: " + health;
+        spriteRend.material = matBlink;
+        Invoke("ResetMaterial", .1f);
+    }
+    private void ResetMaterial()
+    {
+        spriteRend.material = matDefault;
     }
 }
